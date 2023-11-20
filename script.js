01694be02cd06bf6cb20d98a4b6c2c7c9835edbd
Kupigabi <gabor.kupeczki@gmail.com>
2023-11-20 12:51:56 +0100

    practice

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector("#root").insertAdjacentHTML("beforeend", `
+
+<form>
+<h1>Register your pet</h1>
+
+<input type="text" placeholder="pet name" />
+
+<textarea placeholder="symptoms..."></textarea>
+
+<button>submit</button>
+  <div>
+<input type="checkbox" />
+<p>Elolvastam a feltételeket</p>
+</div>
+</form>`)
